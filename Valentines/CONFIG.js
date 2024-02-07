ff869ac961a7a1941ab68c14c8c9d56c81052942
Iannjau <88575466+Iannjau@users.noreply.github.com>
2024-02-07 12:21:12 +0300

    Added new files

---
+++ Valentines/CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hi Baby. </br> Will You Be My Vallentine?",
+  descriptionHeader:
+    "There is no one else I would rather want to be with than you.",
+  buttonYes: "Yes",
+  buttonNo: "No",
+  titleModar: "Thank You For Saying Yes.",
+  descriptionModar:
+    "I love you more than words can say. You are my joy and you make me happy in every way. Every day with you is a blessing and every moment with you is a treasure. Thank you for being my valentine!",
+};
